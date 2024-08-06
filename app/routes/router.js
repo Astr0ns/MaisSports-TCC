29ccc16517780db83b1978f4326903f5c541c47d
Astr0ns <samuelheitor685@gmail.com>
2024-08-06 10:16:03 -0300

    update

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var router = express.Router();
+const bcrypt = require("bcrypt");
+var salt = bcrypt.genSaltSync(12);
+var connection = require("../../config/pool_conexoes");
 
 const {
     verificarUsuAutenticado,
@@ -35,22 +38,47 @@ router.get("/product-page-2", function (req, res) {
     var email = req.session.email;
     res.render("pages/product-page-2", { email: email });
 });
-router.get("/profile", verificarUsuAutorizado([1, 2, 3], "pages/restrito"), async function (req, res) {
-    usuarioController.mostrarPerfil(req, res);
+router.get("/profile", async function (req, res) {
     var nome = req.session.nome;
     var email = req.session.email;
+    var autenticado = req.session.autenticado;
     console.log(nome);
-    res.render("pages/profile", { nome: nome, email: email });
-}
-);
+    res.render("pages/profile", { nome: nome, email: email, autenticado: autenticado });
+});
+
 
 router.get("/register", function (req, res) {
-    res.render("pages/cadastro", {
+    res.render("pages/register", {
         listaErros: null,
         dadosNotificacao: null,
         valores: { nome_usu: "", nomeusu_usu: "", email_usu: "", senha_usu: "" },
     });
 });
+
+router.post("/fazerRegistro", async function (req, res){
+    const { nome, sobrenome, email, senha, cSenha } = req.body;
+
+    if (senha != cSenha) {
+        req.flash("msg", "As senhas não coicidem");
+        return res.render('/register');
+    }
+
+    const hash = await bcrypt.hash(senha, 10);
+
+    try {
+        const emailExist = await connection.query("SELECT id_cliente FROM usuario_clientes WHERE email = ?", [email]);
+
+        await connection.query("INSERT INTO usuario_clientes (nome, sobrenome, email, senha) VALUES (?, ?, ?, ?)", [nome, sobrenome, email, hash]); {
+            res.render('pages/login', { pagina: "login", logado: null });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(400).send(error.message);
+    }
+    // Verificar se os dados estão chegando corretamente
+});
+
+
 
 
 
@@ -108,16 +136,16 @@ router.post("/fazerLogin", async function (req, res) {
 
 
     try {
-        const account = await connection.query("SELECT * FROM usuario_clientes WHERE email_cliente = ?", [email]);
+        const account = await connection.query("SELECT * FROM usuario_clientes WHERE email = ?", [email]);
 
         if (account[0].length > 0) {
             // Usuário encontrado e senha corresponde
-            req.session.nome = account[0][0].nome_cliente;
-            req.session.sobrenome = account[0][0].sobrenome_cliente;
-            req.session.email = account[0][0].email_cliente;
+            req.session.nome = account[0][0].nome;
+            req.session.sobrenome = account[0][0].sobrenome;
+            req.session.email = account[0][0].email;
 
             // verificar se as senhas conheecidem
-            const passwordMatch = bcrypt.compareSync(senha, account[0][0].senha_cliente)
+            const passwordMatch = bcrypt.compareSync(senha, account[0][0].senha)
 
             if (!passwordMatch) {
                 req.flash('msg', "As senhas não conferem");
