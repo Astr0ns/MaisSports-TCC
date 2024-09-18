dfca0d9ce766ac39a099770cf6943c54e8dce49d
RuhanVilanova <115844918+RuhanVilanova@users.noreply.github.com>
2024-09-18 12:05:49 +0000

    dsdsds

--- app/routes/productRouter.js
+++ app/routes/productRouter.js
@@ -4,9 +4,9 @@ const produtoController = require('../controllers/produtoController');
 const upload = require('../models/upload-middleware'); // Middleware para upload de arquivos
 
 // Rota para exibir o formulário de adicionar produto
-router.get('/adicionarProduto', produtoController.exibirFormularioProduto);
+router.get('/adicionarLocais', produtoController.exibirFormularioProduto);
 
 // Rota para processar o formulário de adicionar produto
-router.post('/adicionarProduto', upload.array('images', 8), produtoController.adicionarProduto);
+
 
 module.exports = router;
