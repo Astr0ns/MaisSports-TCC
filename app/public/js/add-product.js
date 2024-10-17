e74facfe7335cb1de4d84c33fb92a4a390b330a9
vilanova-tcc <ruhan.vila10@gmail.com>
2024-10-17 16:54:33 +0000

    vsfd

--- app/public/js/add-product.js
+++ app/public/js/add-product.js
@@ -1,3 +1,84 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const plans = document.querySelectorAll('input[type="radio"]');
+
+    // Garantir que o primeiro plano fique marcado inicialmente
+    const firstPlan = document.querySelector('.plan');
+    const corSelected = 
+    firstPlan.classList.add('selected');
+    firstPlan.querySelector('.bi-circle').style.display = 'none';
+    firstPlan.querySelector('.bi-check-circle-fill').style.display = 'inline-block';
+
+    plans.forEach(plan => {
+        plan.addEventListener('change', function() {
+            // Remove 'selected' class de todos os planos e reseta os ícones
+            document.querySelectorAll('.plan').forEach(p => {
+                p.classList.remove('selected');
+                p.querySelector('.bi-circle').style.display = 'inline-block';
+                p.querySelector('.bi-check-circle-fill').style.display = 'none';
+                p.style.backgroundColor = "initial"; // Resetando a cor do plano
+            });
+
+            // Adiciona 'selected' class ao plano selecionado
+            const selectedPlanElement = this.parentElement;
+            selectedPlanElement.classList.add('selected');
+            selectedPlanElement.querySelector('.bi-circle').style.display = 'none';
+            selectedPlanElement.querySelector('.bi-check-circle-fill').style.display = 'inline-block';
+            selectedPlanElement.style.backgroundColor = corSelected; // Define a cor do plano selecionado
+        });
+    });
+});
+
+function selectBlack() {
+    // Mudar o background do body
+    document.body.style.backgroundColor = "#010c1aa9";
+    
+    // Mudar o background da seção assinatura-data
+    document.querySelector(".card").style.backgroundColor = "#010c1a8e";
+
+    // assinatura informações
+    document.querySelector(".assinatura-info").style.backgroundColor = "#010c1a";
+    document.querySelector(".assinatura-info").style.color = "#fff";
+    document.querySelector(".assinatura-info h2").style.color = "#f72ba5";
+
+    document.querySelector(".assinatura-info h3 i").style.color = "#f72ba5";
+
+    document.querySelector(".assinatura-data").style.backgroundColor = "rgb(4, 20, 41)";
+    document.querySelector(".plan").style.backgroundColor = "rgb(4, 20, 41)";
+
+    const corSelctedBlack = "rgb(17, 53, 100)";
+    document.querySelector("#card-black").style.display = "flex";
+    document.querySelector("#card-medio").style.display = "none";
+    document.querySelector("#card-basico").style.display = "none";
+}
+
+function selectMedio() {
+    // Mudar o background do body
+    document.body.style.backgroundColor = "#010c1aa9";
+    
+    // Mudar o background da seção assinatura-data
+    document.querySelector(".card").style.backgroundColor = "#010c1a8e";
+
+    // assinatura informações
+    document.querySelector(".assinatura-info").style.backgroundColor = "#010c1a";
+    document.querySelector(".assinatura-info").style.color = "#fff";
+    document.querySelector(".assinatura-info h2").style.color = "#f72ba5";
+
+    document.querySelector(".assinatura-info h3 i").style.color = "#f72ba5";
+
+    document.querySelector(".assinatura-data").style.backgroundColor = "rgb(4, 20, 41)";
+    document.querySelector(".plan").style.backgroundColor = "rgb(4, 20, 41)";
+
+    const corSelctedmedio = "#fff";
+    document.querySelector("#card-black").style.display = "none";
+    document.querySelector("#card-medio").style.display = "flex";
+    document.querySelector("#card-basico").style.display = "none";
+}
+
+function selectNormal() {
+    document.body.style.backgroundColor = "#3A4A5A";
+    document.getElementById("assinaturaData").style.backgroundColor = "#2C3A4A";
+}
+
 
 document.addEventListener("DOMContentLoaded", function() {
     const tipoProdSelect = document.querySelector('.tipo_prod');
