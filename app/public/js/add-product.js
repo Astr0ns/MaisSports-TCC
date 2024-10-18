8e05c52d88de3f1b6110513d2349333e0018ab48
vilanova-tcc <ruhan.vila10@gmail.com>
2024-10-18 12:03:04 +0000

    .38

--- app/public/js/add-product.js
+++ app/public/js/add-product.js
@@ -47,6 +47,10 @@ function selectBlack() {
 
     document.querySelector(".assinatura-data").style.backgroundColor = "rgb(4, 20, 41)";
     document.querySelector(".plan").style.backgroundColor = "rgb(4, 20, 41)";
+    const prices = document.querySelectorAll(".plan .price");
+    prices.forEach(price => {
+        price.style.color = "#fff";
+    });
 
     const corSelctedBlack = "rgb(17, 53, 100)";
     document.querySelector('.corSelected').innerHTML = corSelctedBlack
@@ -78,6 +82,10 @@ function selectMedio() {
 
     document.querySelector(".assinatura-data").style.backgroundColor = "rgb(4, 20, 41)";
     document.querySelector(".plan").style.backgroundColor = "rgb(4, 20, 41)";
+    const prices = document.querySelectorAll(".plan .price");
+    prices.forEach(price => {
+        price.style.color = "#fff";
+    });
 
     const corSelctedBlack = "rgb(11, 80, 170)";
     document.querySelector('.corSelected').innerHTML = corSelctedBlack
@@ -109,6 +117,10 @@ function selectNormal() {
 
     document.querySelector(".assinatura-data").style.backgroundColor = "#f5f5f5";
     document.querySelector(".plan").style.backgroundColor = "#f5f5f5";
+    const prices = document.querySelectorAll(".plan .price");
+    prices.forEach(price => {
+        price.style.color = "#000";
+    });
 
     const corSelctedBlack = "rgba(107, 107, 107, .1)";
     document.querySelector('.corSelected').innerHTML = corSelctedBlack
@@ -143,21 +155,32 @@ function saberPreco(valorDia) {
     });
 }
 
-function NextAddProduct() {
+function NextAddProduct(num_page) {
     // Esconder a seção atual
-    document.querySelector('.assinatura-page').classList.add('desativar_container');
-    document.querySelector('.assinatura-page').classList.remove('assinatura-page');
+    if(num_page == 1){
+        document.querySelector('.assinatura-page').classList.add('desativar_container');
+        document.querySelector('.assinatura-page').classList.remove('assinatura-page');
 
-    document.querySelector('#product-card-stl').classList.add('product-card');
-    document.querySelector('#product-card-stl').classList.remove('desativar_container');
-    
-    document.querySelector('#product-info-stl').classList.add('product-info');
-    document.querySelector('#product-info-stl').classList.remove('desativar_container');
+        document.querySelector('#product-card-stl').classList.add('product-card');
+        document.querySelector('#product-card-stl').classList.remove('desativar_container');
+        
+        document.querySelector('#product-info-stl').classList.add('product-info');
+        document.querySelector('#product-info-stl').classList.remove('desativar_container');
 
-    document.body.style.backgroundColor = "#fff";
+        document.body.style.backgroundColor = "#fff";
+        document.querySelector(".card").style.backgroundColor = "#f5f5f5";
+
+    } else if (num_page == 2){
+
+        document.querySelector('#product-card-stl').classList.remove('product-card');
+        document.querySelector('#product-card-stl').classList.add('desativar_container');
+        
+        document.querySelector('#product-info-stl').classList.remove('product-info');
+        document.querySelector('#product-info-stl').classList.add('desativar_container');
+
+        document.querySelector(".finalizar-add-product").style.display = "flex";
+    }
     
-    // Mudar o background da seção assinatura-data
-    document.querySelector(".card").style.backgroundColor = "#f5f5f5";
 
 }
 
@@ -176,5 +199,34 @@ document.addEventListener("DOMContentLoaded", function() {
             roupaSelect.selectedIndex = 0; // Reseta para "Selecione"
         }
     });
+    roupaSelect.addEventListener('change', function() {
+        const selectedValue = this.value;
+
+        if (selectedValue === 'calcados') {
+            document.querySelector(".tam-roupa").style.display = "none";
+            document.querySelector(".tam-calcado").style.display = "grid";
+            document.querySelector(".tam-inf").style.display = "block";
+            
+            console.log('Você escolheu calçados.');
+            // Adicione o código que você tem para calçados aqui
+
+        } else if (selectedValue === 'acessorio') {
+            document.querySelector(".tam-calcado").style.display = "none";
+            document.querySelector(".tam-inf").style.display = "none";
+            document.querySelector(".tam-roupa").style.display = "none";
+            console.log('Você escolheu um acessório.');
+            return
+            // Adicione o código que você tem para acessórios aqui
+
+        } else if (selectedValue !== ""){
+            document.querySelector(".tam-calcado").style.display = "none";
+            document.querySelector(".tam-inf").style.display = "block";
+            document.querySelector(".tam-roupa").style.display = "grid";
+            console.log('Você escolheu uma peça de roupa diferente.');
+            // Adicione o código que você tem para outras peças de roupa aqui
+        }
+    });
 });
+
+
 
