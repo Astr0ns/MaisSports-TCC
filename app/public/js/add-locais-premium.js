dd3b2d56e638709c3bc2f0d3347d3f646ad3bf4c
RuhanVilanova <115844918+RuhanVilanova@users.noreply.github.com>
2024-10-28 08:49:09 -0300

    Add files via upload

---
+++ app/public/js/add-locais-premium.js
@@ -0,0 +1,102 @@
+function proximaPag(){
+    document.querySelector('.product-info').style.display = 'none'
+    document.querySelector('.product-card').style.display = 'none'
+
+    document.querySelector('.funcionamento').style.display = 'flex'
+}
+
+function voltarPag(){
+    document.querySelector('.product-info').style.display = 'block'
+    document.querySelector('.product-card').style.display = 'grid'
+
+    document.querySelector('.funcionamento').style.display = 'none'
+}
+
+function toggleButtons(dia){
+    const toggleButton = document.querySelector(`.${dia} .toggle-btn`);
+        
+        if (toggleButton.classList.contains('active')) {
+            toggleButton.textContent = 'FECHADO';
+            document.querySelector(`.${dia} .desativa_pick`).style.display = 'none';
+            toggleButton.classList.toggle('active');
+        } else {
+            
+            toggleButton.textContent = 'ABERTO';
+            document.querySelector(`.${dia} .desativa_pick`).style.display = 'flex';
+            toggleButton.classList.toggle('active');
+        }
+ }
+
+ // Função para mostrar o modal
+function mostrarEscolherHora(dia) {
+            
+    document.querySelector(`.${dia} .overlay`).style.display = 'block';
+    document.querySelector(`.${dia} .escolher_hora`).style.display = 'flex';
+    
+}
+
+// Função para esconder o modal
+function esconderEscolherHora(dia) {
+    const toggleButton = document.querySelector(`.${dia} .toggle-btn`);
+    document.querySelector(`.${dia} .overlay`).style.display = 'none';
+    document.querySelector(`.${dia} .escolher_hora`).style.display = 'none';
+
+    if (toggleButton.classList.contains('active')) {
+        const iniHora = document.querySelector(`.${dia} #start-time`).value
+        const fimHora = document.querySelector(`.${dia} #end-time`).value
+        
+        document.querySelector(`.${dia} .h3_hora`).innerHTML = `${iniHora} - ${fimHora}`;
+    } else {
+        document.querySelector(`.${dia} .h3_hora`).innerHTML = `FECHADO`;
+    }
+}
+
+window.onload = function () {
+    const lat = localStorage.getItem('latitude');
+    const lng = localStorage.getItem('longitude');
+
+    if (lat && lng) {
+        document.getElementById('latitude').value = lat;
+        document.getElementById('longitude').value = lng;
+        initMap(parseFloat(lat), parseFloat(lng));
+    }
+};
+
+function initMap(lat, lng) {
+    const location = { lat: lat, lng: lng };
+    const map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: location
+    });
+    
+    var styles = {
+        default: null,
+        hide: [
+            {
+            featureType: 'poi.business',
+            stylers: [{visibility: 'off'}]
+        },
+        {
+        featureType: 'transit',
+        elementType: 'labels.icon',
+        stylers: [{visibility: 'off'}]
+      }
+    ]
+  };
+
+  map.setOptions({styles: styles['hide']});
+
+    // Adiciona o marcador na localização especificada
+    new google.maps.Marker({
+        position: location,
+        map: map,
+        icon: {
+            url: 'imagem/pincrown9.png',
+            scaledSize: new google.maps.Size(45, 45)
+        }
+    });
+}
+
+
+
+
---
+++ app/public/js/cep-api.js
@@ -0,0 +1,86 @@
+$(document).ready(function() {
+
+    function limpa_formulário_cep() {
+        // Limpa valores do formulário de cep.
+        $("#rua").val("");
+        $("#bairro").val("");
+        $("#cidade").val("");
+        $("#uf").val("");
+        $("#ibge").val("");
+    }
+    
+    //Quando o campo cep perde o foco.
+    $("#cep").blur(function() {
+
+        //Nova variável "cep" somente com dígitos.
+        var cep = $(this).val().replace(/\D/g, '');
+
+        //Verifica se campo cep possui valor informado.
+        if (cep != "") {
+
+            //Expressão regular para validar o CEP.
+            var validacep = /^[0-9]{8}$/;
+
+            //Valida o formato do CEP.
+            if(validacep.test(cep)) {
+
+                //Preenche os campos com "..." enquanto consulta webservice.
+                $("#rua").val("...");
+                $("#bairro").val("...");
+                $("#cidade").val("...");
+                $("#uf").val("...");
+                $("#ibge").val("...");
+
+                //Consulta o webservice viacep.com.br/
+                $.getJSON("https://viacep.com.br/ws/"+ cep +"/json/?callback=?", function(dados) {
+
+                    if (!("erro" in dados)) {
+                        //Atualiza os campos com os valores da consulta.
+                        $("#rua").val(dados.logradouro);
+                        $("#bairro").val(dados.bairro);
+                        $("#cidade").val(dados.localidade);
+                        $("#uf").val(dados.uf);
+                        $("#ibge").val(dados.ibge);
+                    } //end if.
+                    else {
+                        //CEP pesquisado não foi encontrado.
+                        limpa_formulário_cep();
+                        alert("CEP não encontrado.");
+                    }
+                });
+            } //end if.
+            else {
+                //cep é inválido.
+                limpa_formulário_cep();
+                alert("Formato de CEP inválido.");
+            }
+        } //end if.
+        else {
+            //cep sem valor, limpa formulário.
+            limpa_formulário_cep();
+        }
+    });
+});
+
+function limpa_formulari_cep(){
+    // limpa valores do formulario
+    document.getElementById('rua').value= ("");
+    document.getElementById('bairro').value= ("");
+    document.getElementById('cidade').value= ("");
+    document.getElementById('uf').value= ("");
+}
+
+function meu_callback(conteudo){
+    if(!("erro" in conteudo)){
+        // Atualiza os campos com os valores
+        document.getElementById('rua').value= (conteudo.logradouro);
+    document.getElementById('bairro').value= (conteudo.bairro);
+    document.getElementById('cidade').value= (conteudo.localidade);
+    document.getElementById('uf').value= (conteudo.uf);
+    } //end if
+    else{
+        //CEP não encontrado
+        limpa_formulario_cep();
+        notify("Aviso", "CEP não encontrado", "Warning", "center")
+    }
+}
